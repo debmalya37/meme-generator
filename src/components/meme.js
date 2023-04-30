9cca436d9244381b87ce9dee32657429f787677b
Debmalya <debmalyasen37@gmail.com>
2023-04-30 13:08:07 +0530

    added data.js and make the get meme image button functionable with the useState and connected to the API fetched data

--- src/components/meme.js
+++ src/components/meme.js
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import { useState}  from 'react';
+import memesData from "./data";
 export default function Meme() {
+    const [memeImage, setMemeImage] = useState("")
+
+
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
+    }
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input
                      type="text"
                      placeholder='Top Text'
@@ -14,11 +24,12 @@ export default function Meme() {
                      placeholder='Bottom Text'
                      className="form--input" 
                 />
-                <button className="form--button">
+                <button className="form--button"
+                onClick={getMemeImage}>
                 😜Get a new Meme Image 🖼️
                 </button>
-
-            </form>
+                <img src={memeImage} alt="" className='meme--image'/>
+            </div>
         </main>
         
         
